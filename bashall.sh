9adb98e03d7481d573e6e52cee0e784b5badb377
AxenoDev <93048526+AxenoDev@users.noreply.github.com>
2022-06-20 20:10:35 +0200

    Updata Bashall.sh

--- bashall.sh
+++ bashall.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-vert='\e[1;32m'
-echo "${vert}Recherche de mis à jours du script ..."
+vertclair='\e[1;32m'
+
+echo -e "${vertclair}Recherche de mis à jours du script ..."
 sleep 3
 rm -rf script.sh
 wget https://raw.githubusercontent.com/AxenoDev/PteroInstall/main/script.sh
